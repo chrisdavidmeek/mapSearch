5d8ac97dfb4a3f3e9644a015dfd32a521d082b1d
Chris Meek <meekch@iu.edu>
2020-04-19 12:50:05 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,48 +1,66 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import GoogleMapReact from "google-map-react";
 
 function App() {
-  const [position, setPosition] = React.useState({}); //holds x and y positions
-  const [error, setError] = React.useState(null);
-  const [note, setNote] = React.useState("Not close to the gym");
-  const [arrived, setArrive] = React.useState(false);
+  const iupui = [39.773856, -86.177997];
+  const albany = [42.652345, -73.75817];
+  const seattle = [47.60461, -122.335205];
+  const annArbor = [42.280412, -83.741811];
+  const myLoc = [];
+  const [location, setLocation] = React.useState(iupui);
 
-  const gym = { x: 39.768996942, y: -86.17166598 };
+  const [newX, setNewX] = React.useState(0);
+  const [newY, setNewY] = React.useState(0);
 
-  const onChange = ({ coords }) => {
-    setPosition({
-      x: coords.latitude,
-      y: coords.longitude,
-    });
+  const changeX = (e) => {
+    setNewX(e);
+    myLoc.push(e);
   };
-  const onError = (error) => {
-    setError(error.message);
-  };
-
-  const changeButton = () => {
-    setArrive(!arrived);
+  const changeY = (e) => {
+    setNewY(e);
+    myLoc.push(e);
   };
 
-  React.useEffect(() => {
-    const geo = navigator.geolocation; //db based off of current location
-
-    if (position.x === gym.x && position.y === gym.y) {
-      setNote("You have arrived");
-    } else {
-    }
-
-    let watcher = geo.watchPosition(onChange, onError);
-    return () => geo.clearWatch(watcher);
-  }, [arrived]);
+  const pushChanges = () => {
+    myLoc.push(newX, newY);
+  };
+  const setNewLocation = () => {
+    pushChanges();
+    setLocation(myLoc);
+    console.log(myLoc);
+  };
 
   return (
     <div className="App">
-      <h1>{position.x}</h1>
-      <br />
-      <h1>{position.y}</h1>
-      <div>{note}</div>
-      <button onClick={changeButton}>Click to see</button>
+      <h2> Warp to </h2>
+      <button onClick={() => setLocation(albany)}>Albany, New York</button>
+      <button onClick={() => setLocation(seattle)}>Seattle, Washington</button>
+      <button onClick={() => setLocation(annArbor)}>Ann Arbor, Michigan</button>
+      <h2> Try your own locations!</h2>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Input a latitude"
+          onChange={(e) => changeX(Number(e.target.value))}
+        ></input>
+        <input
+          type="text"
+          placeholder="Input a longitude"
+          onChange={(e) => changeY(Number(e.target.value))}
+        ></input>
+      </form>
+      <button onClick={() => setNewLocation()}>Submit</button>
+
+      <div style={{ height: "600px", width: "auto" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyAroi5UF-gCX8V1vi44aApurvJEiBKJEos" }}
+          center={location}
+          defaultZoom={16}
+          distanceToMouse={() => {}}
+        ></GoogleMapReact>
+      </div>
     </div>
   );
 }
